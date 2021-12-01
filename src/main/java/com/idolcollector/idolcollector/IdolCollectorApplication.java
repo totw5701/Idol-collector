970368a84017e7daa8571b5d2254e2e7e4180735
totw5701 <76473868+totw5701@users.noreply.github.com>
2021-12-01 18:34:24 +0900

    백엔드 환경 구성2
    DB연결 및 Spring Security 꺼둠.

--- src/main/java/com/idolcollector/idolcollector/IdolCollectorApplication.java
+++ src/main/java/com/idolcollector/idolcollector/IdolCollectorApplication.java
@@ -2,8 +2,9 @@ package com.idolcollector.idolcollector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class IdolCollectorApplication {
 
     public static void main(String[] args) {
