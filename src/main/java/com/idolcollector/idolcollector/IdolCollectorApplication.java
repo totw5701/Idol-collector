1f15f029816b5f0aaa9b9d0d63e11e364e1bbbf2
totw5701 <76473868+totw5701@users.noreply.github.com>
2021-12-01 17:58:47 +0900

    백엔드 환경 구성
    Lombok
    
    Spring web
    
    Spring Security
    
    Spring Data JPA
    
    Mysql
    
    Thymleaf

---
+++ src/main/java/com/idolcollector/idolcollector/IdolCollectorApplication.java
@@ -0,0 +1,13 @@
+package com.idolcollector.idolcollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdolCollectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdolCollectorApplication.class, args);
+    }
+
+}
