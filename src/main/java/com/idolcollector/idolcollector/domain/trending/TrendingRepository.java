dad0020078c5a271f2e6276e9410351abb7da139
totw5701 <76473868+totw5701@users.noreply.github.com>
2021-12-06 17:48:29 +0900

    Trending Repository 버그 수정

--- src/main/java/com/idolcollector/idolcollector/domain/trending/TrendingRepository.java
+++ src/main/java/com/idolcollector/idolcollector/domain/trending/TrendingRepository.java
@@ -2,5 +2,5 @@ package com.idolcollector.idolcollector.domain.trending;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TrendingRepository extends JpaRepository {
+public interface TrendingRepository extends JpaRepository<Trending, Long> {
 }
