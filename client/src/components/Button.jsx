2f64fe68bd2b5804400482c5f7375a6c1c1cd685
chwonseok <dev.chwonseok@gmail.com>
2021-12-10 15:38:49 +0900

    Modify: ButtonJSX에 hover이펙트 추가

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -10,9 +10,12 @@ const StyledButton = styled.button`
   cursor: pointer;
   border: none;
   background: transparent;
+  padding: 5px 10px;
+  border-radius: 8px;
 
   font-size: 1.1rem;
 
   &:hover {
+    background: rgb(240, 240, 240);
   }
 `;
