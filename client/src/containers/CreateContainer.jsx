0fa063d96bb66f98e3f5eb981dc65e9054ddebd5
chwonseok <dev.chwonseok@gmail.com>
2021-12-23 02:11:10 +0900

    Modify: Create페이지 좌측 디자인

--- client/src/containers/CreateContainer.jsx
+++ client/src/containers/CreateContainer.jsx
@@ -3,7 +3,12 @@ import styled from 'styled-components';
 function UploadContainer() {
   return (
     <UploadWrap>
-      <UploadLeft>THIS IS AREA WHERE USER SELECT THEIR PHOTO</UploadLeft>
+      <UploadLeft>
+        <TrashBin src="images/휴지통.png" />
+        <h2>업로드 하기</h2>
+        <SelectImg src="images/이미지.png" />
+        <h3>권장파일사양: </h3>
+      </UploadLeft>
       <CenterLine />
       <UploadRight>Form area</UploadRight>
     </UploadWrap>
@@ -25,11 +30,29 @@ const UploadLeft = styled.div`
   width: 40%;
   height: 100%;
 
+  position: relative;
+
   border-top: none;
   border-left: none;
   border-radius: 30px;
   -webkit-box-shadow: 5px 5px 5px 0px rgba(0, 0, 0, 0.2);
   box-shadow: 5px 5px 5px 0px rgba(0, 0, 0, 0.2);
+
+  h2 {
+    color: #6f6f6f;
+    position: absolute;
+    left: 50%;
+    top: 35%;
+    transform: translate(-50%);
+  }
+
+  h3 {
+    color: #6f6f6f;
+    position: absolute;
+    left: 50%;
+    top: 80%;
+    transform: translate(-50%);
+  }
 `;
 
 const CenterLine = styled.div`
@@ -43,4 +66,33 @@ const UploadRight = styled.div`
   width: 40%;
   height: 100%;
   background: lightgrey;
+`;
+
+const TrashBin = styled.img`
+  width: 2rem;
+  height: 2rem;
+
+  position: absolute;
+  right: 1rem;
+  top: 1rem;
+
+  &:hover {
+    cursor: pointer;
+    opacity: 0.8;
+  }
+`;
+
+const SelectImg = styled.img`
+  height: 5rem;
+  width: 6.5rem;
+
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+
+  &:hover {
+    cursor: pointer;
+    opacity: 0.8;
+  }
 `;
