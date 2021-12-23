fa2930f54514b7039ad8b367d655899c1a933568
chwonseok <dev.chwonseok@gmail.com>
2021-12-23 20:35:23 +0900

    Add: 사진 업로드 기능 in CreateContainerJSX - 사진 업로드 그리고 삭제 기능

--- client/src/containers/CreateContainer.jsx
+++ client/src/containers/CreateContainer.jsx
@@ -1,13 +1,54 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 
 function UploadContainer() {
+  const [isSelected, setIsSelected] = useState(false);
+  const [selectedPhoto, setSelectedPhoto] = useState({
+    photo: [],
+    photoPreview: null,
+  });
+
+  const handleDeletePhoto = e => {
+    setIsSelected(false);
+    console.log(selectedPhoto);
+  };
+
+  const handleSelectPhoto = e => {
+    setIsSelected(true);
+    setSelectedPhoto({
+      ...selectedPhoto,
+      photo: e.target.files[0],
+      photoPreview: URL.createObjectURL(e.target.files[0]),
+    });
+  };
+
   return (
     <UploadWrap>
       <UploadLeft>
-        <TrashBin src="images/휴지통.png" />
-        <h2>업로드 하기</h2>
-        <SelectImg src="images/이미지.png" />
-        <h3>권장파일사양: </h3>
+        {isSelected ? (
+          <>
+            <DeleteBtn onClick={handleDeletePhoto}>
+              <img src="images/휴지통.png" alt="delete preview card button" />
+            </DeleteBtn>
+            <PhotoHolder>
+              <img src={selectedPhoto.photoPreview} alt="selected card" />
+            </PhotoHolder>
+          </>
+        ) : (
+          <>
+            <input
+              style={{ display: 'none' }}
+              id="card-input"
+              type="file"
+              accept="image/*"
+              onChange={handleSelectPhoto}
+            />
+            <SelectImgLabel htmlFor="card-input">
+              <img src="images/이미지.png" alt="uploading card" />
+            </SelectImgLabel>
+            <h3>권장파일사양: </h3>
+          </>
+        )}
       </UploadLeft>
       <CenterLine />
       <UploadRight>Form area</UploadRight>
@@ -38,20 +79,29 @@ const UploadLeft = styled.div`
   -webkit-box-shadow: 5px 5px 5px 0px rgba(0, 0, 0, 0.2);
   box-shadow: 5px 5px 5px 0px rgba(0, 0, 0, 0.2);
 
-  h2 {
-    color: #6f6f6f;
-    position: absolute;
-    left: 50%;
-    top: 35%;
-    transform: translate(-50%);
-  }
-
   h3 {
     color: #6f6f6f;
     position: absolute;
     left: 50%;
     top: 80%;
     transform: translate(-50%);
+  }
+`;
+
+const PhotoHolder = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: #d6d5d5;
+  border-radius: 30px;
+
+  img {
+    max-width: 99.8%;
+    max-height: 100%;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
   }
 `;
 
@@ -68,24 +118,28 @@ const UploadRight = styled.div`
   background: lightgrey;
 `;
 
-const TrashBin = styled.img`
-  width: 2rem;
-  height: 2rem;
-
+const DeleteBtn = styled.div`
   position: absolute;
   right: 1rem;
   top: 1rem;
+  z-index: 1;
+
+  padding: 0.2rem;
+  background: #fff;
+  border-radius: 10px;
 
   &:hover {
     cursor: pointer;
     opacity: 0.8;
   }
+
+  img {
+    width: 1.5rem;
+    height: 1.5rem;
+  }
 `;
 
-const SelectImg = styled.img`
-  height: 5rem;
-  width: 6.5rem;
-
+const SelectImgLabel = styled.label`
   position: absolute;
   left: 50%;
   top: 50%;
@@ -94,5 +148,10 @@ const SelectImg = styled.img`
   &:hover {
     cursor: pointer;
     opacity: 0.8;
+  }
+
+  img {
+    height: 4rem;
+    width: 5rem;
   }
 `;
