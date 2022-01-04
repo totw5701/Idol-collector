fc91c8ca703469c4c1084a56dc60a7798303ad24
totw5701 <76473868+totw5701@users.noreply.github.com>
2022-01-04 21:04:53 +0900

    예외처리 디폴트 JSON 쓰도록 변경

--- src/main/java/com/idolcollector/idolcollector/advice/ExControllerAdvice.java
+++ src/main/java/com/idolcollector/idolcollector/advice/ExControllerAdvice.java
@@ -4,8 +4,11 @@ import com.idolcollector.idolcollector.advice.exception.CBundleNotFoundException
 import com.idolcollector.idolcollector.advice.exception.CMemberNotFoundException;
 import com.idolcollector.idolcollector.advice.exception.CNotLoginedException;
 import com.idolcollector.idolcollector.advice.exception.CPostNotFoundException;
+import com.idolcollector.idolcollector.service.ResponseService;
 import com.idolcollector.idolcollector.web.dto.BindingError;
 import com.idolcollector.idolcollector.web.dto.ErrorResult;
+import com.idolcollector.idolcollector.web.dto.response.CommonResult;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
@@ -19,48 +22,52 @@ import java.util.List;
 
 @Slf4j
 @RestControllerAdvice
+@RequiredArgsConstructor
 public class ExControllerAdvice {
+
+    private final ResponseService responseService;
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler
-    public ErrorResult exHandler(Exception e) {
+    public CommonResult exHandler(Exception e) {
         log.error("[exceptionHandler} ex", e);
-        return new ErrorResult(-100, "알 수 없는 내부 오류가 발생했습니다.");
+        return responseService.getFailResult(-100, "알 수 없는 내부 오류가 발생했습니다.");
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler
-    public ErrorResult cMemberNotFoundException(CMemberNotFoundException e) {
+    public CommonResult cMemberNotFoundException(CMemberNotFoundException e) {
         log.error("[exceptionHandler} ex", e);
-        return new ErrorResult(-101, "존재하지 않은 회원입니다.");
+        return responseService.getFailResult(-101, "존재하지 않은 회원입니다.");
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler
-    public ErrorResult cPostNotFoundException(CPostNotFoundException e) {
+    public CommonResult cPostNotFoundException(CPostNotFoundException e) {
         log.error("[exceptionHandler} ex", e);
-        return new ErrorResult(-102, "존재하지 않은 카드입니다.");
+        return responseService.getFailResult(-102, "존재하지 않은 카드입니다.");
+
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler
-    public ErrorResult cBundleNotFoundException(CBundleNotFoundException e) {
+    public CommonResult cBundleNotFoundException(CBundleNotFoundException e) {
         log.error("[exceptionHandler} ex", e);
-        return new ErrorResult(-103, "존재하지 않은 카드집입니다.");
+        return responseService.getFailResult(-103, "존재하지 않은 카드집입니다.");
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler
-    public ErrorResult cNotLoginedException(CNotLoginedException e) {
+    public CommonResult cNotLoginedException(CNotLoginedException e) {
         log.error("[exceptionHandler} ex", e);
-        return new ErrorResult(-200, "로그인 되어있지 않습니다.");
+        return responseService.getFailResult(-200, "로그인 되어있지 않습니다.");
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler
-    public ErrorResult accessDeniedException(AccessDeniedException e) {
+    public CommonResult accessDeniedException(AccessDeniedException e) {
         log.error("[exceptionHandler} ex", e);
-        return new ErrorResult(-201, "보유 권한으로 접근할 수 없습니다.");
+        return responseService.getFailResult(-201, "보유 권한으로 접근할 수 없습니다.");
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
@@ -72,17 +79,20 @@ public class ExControllerAdvice {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler
-    public BindingError validationExHandler(BindException e) {
+    public CommonResult validationExHandler(BindException e) {
         log.error("[exceptionHandler} ex", e);
         FieldError err = e.getFieldError();
-        return new BindingError(err.getObjectName(), err.getField(), err.getCode(), err.getDefaultMessage());
+        return responseService.getFailResult(-301, String.valueOf(err.getObjectName()) + " " + String.valueOf(err.getField()) + err.getDefaultMessage());
+        //return new BindingError(err.getObjectName(), err.getField(), err.getCode(), err.getDefaultMessage());
 
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler
-    public ErrorResult reqParamExHandler(MissingServletRequestParameterException e) {
+    public CommonResult reqParamExHandler(MissingServletRequestParameterException e) {
         log.error("[exceptionHandler} ex", e);
-        return new ErrorResult(-1, "파라미터 값이 없습니다.");
+        return responseService.getFailResult(-302, "파라미터 값이 없습니다.");
+
+        //return new ErrorResult(-1, "파라미터 값이 없습니다.");
     }
 }
