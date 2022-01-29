1a066896591cd604cd3c101d9424b2e746a1dbd1
bellasimi <WorkSY1111@gmail.com>
2022-01-29 21:23:54 +0900

    modify: data 디렉토리 분리, 더미데이터 추가

---
+++ client/src/data/dummyBundle.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    code: 200,
+    data: {
+      description: "string",
+        id: 0,
+        memberNickName: "nickname0",
+        posts: [
+          {
+            id: 0,
+            authorId: 'id0',
+            title: 'korean male idol',
+            content: '파란배경 아이돌 사진',
+            createDate: '2021-12-21',
+            views: 100,
+            likes: 10,
+            storeFileName:
+              'https://www.smlounge.co.kr/upload/woman/article/201911/thumb/43390-393024-sampleM.jpg',
+          }
+        ],
+        title: "string"
+      },
+      msg: "성공했습니다.",
+      success: true
+    }
+]
