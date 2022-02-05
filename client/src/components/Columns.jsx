bf7a625c178e3ece3fb9b973b8ed7890bad13439
bellasimi <WorkSY1111@gmail.com>
2022-02-05 18:13:41 +0900

    Feat: api/home/page 무한 스크롤 기능 추가

--- client/src/components/Columns.jsx
+++ client/src/components/Columns.jsx
@@ -1,16 +1,90 @@
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+import React, { useState, useEffect, useCallback } from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import { useInView } from 'react-intersection-observer'
+import ApiService from '../ApiService'
+
 
 function Columns({ data }) {
-  if (!Array.isArray(data)) return null;
+
+
+/*  dummyDB
+  const copy = data.filter(e => e.id < 10)
+  const copy2 = data.filter(e => e.id > 10 && e.id <20)
+  const copy3 = data.filter(e => e.id > 20)
+  const copies = [copy,copy2,copy3]
+  console.log(copies)
+  */
+
+
+
+  const [card, setCard] = useState([])
+  const [page, setPage] = useState( 0 ) //PageRequest page 0부터 시작
+  const [loading,setLoading] = useState( false )
+
+  const [last,inView] = useInView()//ref속성값으로 last 들어가면 inView가 true로 변함
+
+/*   api/home/page: 페이지가 바뀔 때마다 해당 page data받기 */
+  const getData = useCallback(() => {
+    setLoading(true)
+
+    ApiService.getHomePage(page)
+    .then((result) => {
+      console.log(result)
+      setCard([...data,...result])
+      setLoading(false)
+    })
+    .catch((err) => {
+      console.log('/api/home/page axios 에러! '+err )
+      setCard(data)
+      setLoading(false)
+    })
+
+    /* dummyDB로 test
+      if(page<3){
+      const result = copies[page]
+
+      setCard([...card,...result])
+      //console.log('result: '+ JSON.stringify(copies[page]))
+      console.log('page'+page)
+    }*/
+
+  },[page])
+
+  // getData가 바뀔 때마다 실행
+  useEffect(() => {
+    getData()
+    //console.log('카드 개수'+card.length)
+  },[getData])
+
+  // client가 마지막을 보고있고 로딩중이 아닐경우 page를 더해줌
+  useEffect(() => {
+    if( inView && !loading ) {
+      setPage(page+1)
+    }
+  }, [inView,loading])
+
+  if (!Array.isArray(data)) return null
 
   return (
-    <ColumnsContainer>
-      {data.map(el => (
+   <ColumnsContainer>
+      {card.map((el,idx) => (
         <Card key={el.id}>
-          <Link to={`/card/${el.id}`}>
-            <img src={el.storeFileName} alt={el.title} />
-          </Link>
+          { card.length -1 === idx
+            ? (
+                <Link to={`/card/${el.id}`} ref={ last } >
+                  <img src={el.storeFileName} alt={el.title} />
+                </Link>
+
+              )
+            : (
+                <Link to={`/card/${el.id}`}>
+                  <img src={el.storeFileName} alt={el.title} />
+                </Link>
+              )
+
+          }
+
         </Card>
       ))}
     </ColumnsContainer>
