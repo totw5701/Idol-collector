dfac66ff02e378b08dd57492f41152ef1a5f96bc
bellasimi <WorkSY1111@gmail.com>
2022-02-07 20:31:15 +0900

    Chore: axios data 수정

--- client/src/data/axiosPost.js
+++ client/src/data/axiosPost.js
@@ -3,12 +3,11 @@ import ApiService from '../ApiService'
 let axiosPost = []
 
 ApiService.getHome().then(( result ) => {
-  console.log(result)
+  //console.log(result)
   axiosPost = [...axiosPost,...result.data.data.cards]
-  console.log('axiosPost: '+ axiosPost)
 
 }).catch((err) => {
-  console.log('api/home axios 실패!')
+  console.log('api/home axios 실패! '+err )
 
 })
 
