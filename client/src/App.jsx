3242ecfe2d59b09241e67e02f95a2528537fe951
bellasimi <WorkSY1111@gmail.com>
2022-02-10 15:20:22 +0900

    Chore: App import, export 누락 오류 수정

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,25 @@
 import MainPage from './pages/MainPage';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Nav from './components/Nav';
+import TopBtn from './components/TopBtn';
+import DetailPage from './pages/DetailPage';
 import CreatePage from './pages/CreatePage';
 import SettingPage from './pages/SettingPage';
-import { useSelector,useDispatch } from 'react-redux';
-import ApiService from './ApiService'
-import { useSelector, useDispatch } from 'react-redux';
-import ApiService from './ApiService';
-import { useState } from 'react';
+import SearchPage from './pages/SearchPage';
 import LoginPage from './pages/LoginPage';
 import UserPage from './pages/UserPage';
+import { useSelector,useDispatch } from 'react-redux';
+import ApiService from './ApiService'
+import { useState } from 'react';
 
 function App() {
 
   const dispatch = useDispatch()
-  const dispatch = useDispatch();
   const [isLogin, setIsLogin] = useState(false);
 
 /*  post */
    let data = useSelector(({postReducer})=> { return postReducer })
-  /*  post */
-  let data = useSelector(({ postReducer }) => {
-    return postReducer;
-  });
+
   // let data = useSelector((state)=> { return state.postReducer })
 
 /*   let axiosPost = [];
@@ -31,18 +30,6 @@ function App() {
     console.log('axiosPost 데이터 가져오기 에러! '+err )
   }) */
 
-/* member */
-  let axiosMember = {}
-
-  ApiService.getHome().then(( result ) => {
-    //console.log(result.data.data.member)
-    axiosMember = {...axiosMember,...result.data.data.member}
-    //console.log(axiosMember)
-    dispatch({ type:'axiosMember', payload: axiosMember })
-  }).catch((err) => {
-    console.log('axiosMember 데이터 가져오기 에러! '+err )
-
-  })
   /* member */
   let axiosMember = {};
 
@@ -119,3 +106,5 @@ function App() {
     </BrowserRouter>
   );
 }
+
+export default App
