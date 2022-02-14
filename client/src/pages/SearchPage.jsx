5f4da02a147455bd71ee160221e85d20c26f6c4d
bellasimi <WorkSY1111@gmail.com>
2022-02-15 03:21:52 +0900

    Chore: SearchPage css 추가

--- client/src/pages/SearchPage.jsx
+++ client/src/pages/SearchPage.jsx
@@ -6,6 +6,8 @@ import SearchBar from '../components/SearchBar'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import qs from 'qs'
+import styled from 'styled-components';
+import { css } from 'styled-components';
 
 function SearchPage() {
 
@@ -26,17 +28,49 @@ function SearchPage() {
       setAxiosSearch(result.data.data.cards)
     })
     .catch((err) => {
-      console.log('getSearch axios 에러! '+err )
+      console.log('getSearch axios 에러! '+err );
+      console.log(keywords);
     })
 
   },[keywords])
 
   return (
     <>
-      <SearchBar />
+      <StyledBanner>
+        <Banner>'{keywords.keywords}'로 검색한 결과입니다</Banner>
+        <SearchBar />
+      </StyledBanner>
+      <Line/>
       <Columns pageName = 'search' param = { param } />
     </>
   );
 }
 
 export default SearchPage;
+
+const BannerColor = '#b580d1';
+const LineColor = '#b580d1';
+
+const StyledBanner = styled.div`
+  margin: 0 auto 20px auto;
+  padding-top: 65px;
+`;
+
+const Banner = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  margin: 20px auto 50px auto;
+  font-size: 30px;
+  font-weight: 700;
+  color: ${ BannerColor };
+`;
+
+const Line = styled.div`
+  width: 90%;
+  margin: 0 auto 20px auto;
+  height: 4px;
+  background-color: ${ LineColor };
+`;
