6bc815ed0d10b9749d9fb05c2bf70c097cf57fd7
bellasimi <WorkSY1111@gmail.com>
2022-02-17 04:42:22 +0900

    Chore: actions getMember 추가

--- client/src/redux/modules/actions.js
+++ client/src/redux/modules/actions.js
@@ -1,4 +1,6 @@
-import { ADD_LIKE, REMOVE_LIKE, ADD_VIEW } from './types'
+import { ADD_LIKE, REMOVE_LIKE, ADD_VIEW, GET_MEMBER } from './types'
+import ApiService from '../../ApiService'
+import axios from 'axios'
 
 export const addLike = (id) => {
   return {
@@ -22,3 +24,17 @@ export const addView = (id) => {
 
 }
 
+/* 회원 정보 가져오기 */
+export const getMember = async () => {
+
+  let home = await ApiService.getHome()
+  .catch((err) => {
+    console.log('getMember actions 에러! '+err )
+  })
+
+  return {
+    type: GET_MEMBER,
+    payload: home.data.data.member
+  }
+
+}
