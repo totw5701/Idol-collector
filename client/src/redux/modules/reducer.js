52708ba2bfe7d3e07b1cb42793cf8013674c0915
신승민 <shinseungmin10@gmail.com>
2022-03-02 23:47:51 +0900

    add redux

--- client/src/redux/modules/reducer.js
+++ client/src/redux/modules/reducer.js
@@ -1,28 +1,31 @@
-import { combineReducers } from 'redux'
-import { 
-  ADD_LIKE, 
-  REMOVE_LIKE, 
-  ADD_VIEW, 
+import { combineReducers } from 'redux';
+import {
+  ADD_LIKE,
+  REMOVE_LIKE,
+  ADD_VIEW,
   GET_MEMBER,
   USER_CARD,
   USER_SCRAP,
- } from './types'
-import dummyPost from '../../data/dummyPost'
-import axiosPost from '../../data/axiosPost'
+  USER_INFO,
+  USER_PHOTO,
+} from './types';
+import dummyPost from '../../data/dummyPost';
+import axiosPost from '../../data/axiosPost';
 //import axiosMember from '../../data/axiosMember'
-import dummyBundle from '../../data/dummyBundle'
-import dummyMember from '../../data/dummyMember'
-import dummyScrap from '../../data/dummyScrap'
-import dummyUserCard from '../../data/dummyUserCard'
-import ApiService from '../../ApiService'
+import dummyBundle from '../../data/dummyBundle';
+import dummyMember from '../../data/dummyMember';
+import dummyScrap from '../../data/dummyScrap';
+import dummyUserCard from '../../data/dummyUserCard';
+// import dummyPhoto from '../../images'
+import ApiService from '../../ApiService';
 
-const post = dummyPost
+const post = dummyPost;
 // const post = axiosPost
-const bundle = dummyBundle
-const member = dummyMember
-const scrap = dummyScrap
-const usercard = dummyUserCard
-const photo = {}
+const bundle = dummyBundle;
+const member = dummyMember;
+const scrap = dummyScrap;
+const usercard = dummyUserCard;
+const photo = {};
 
 // const userReducer = ( state = member, action) => {
 //   switch(action.type) {
@@ -35,81 +38,83 @@ const photo = {}
 //   }
 // }
 
-
 /* post: mainPage 카드들 */
-const postReducer = ( state = post, action = { type: '' } ) => {
-  let copy = [...state]
+const postReducer = (state = post, action = { type: '' }) => {
+  let copy = [...state];
 
-  switch(action.type) {
-
+  switch (action.type) {
     /* likes */
     case ADD_LIKE:
-      copy[action.id].likes++
-      return copy
+      copy[action.id].likes++;
+      return copy;
 
     case REMOVE_LIKE:
-      copy[action.id].likes--
-      return copy
+      copy[action.id].likes--;
+      return copy;
 
     /* views */
     case ADD_VIEW:
-      copy[action.id].views++
-      return copy
+      copy[action.id].views++;
+      return copy;
 
-
-    default: return state
-
+    default:
+      return state;
   }
-
-}
+};
 
 /* bundle: mypage 카드집 */
-const bundleReducer = ( state = bundle, action = { type: '' } ) => {
-    let copy = [...bundle]
+const bundleReducer = (state = bundle, action = { type: '' }) => {
+  let copy = [...bundle];
 
-    return state
-}
+  return state;
+};
 
 // mypage 카드모음
-const userCardReducer = ( state = usercard, action = { type: '' } ) => {
+const userCardReducer = (state = usercard, action = { type: '' }) => {
   switch (action.type) {
     case USER_CARD:
-      return {...state, userCard: action.payload}
+      return { ...state, userCard: action.payload };
+    default:
+      return state;
   }
-}
+};
 
-const scrapReducer = ( state = scrap, action = { type: '' } ) => {
+// mypage 스크랩모음
+const scrapReducer = (state = scrap, action = { type: '' }) => {
   switch (action.type) {
     case USER_SCRAP:
-      return {...state, userScrap: action.payload}
+      return { ...state, userScrap: action.payload };
+    default:
+      return state;
   }
-}
+};
 
 /* 회원관리 */
-const memberReducer = ( state = member, action = {type: ''} ) => {
-
-  switch(action.type){
-
-    case GET_MEMBER:  return { ...state, userData: action.payload }
-
+const memberReducer = (state = member, action = { type: '' }) => {
+  switch (action.type) {
+    case GET_MEMBER:
+      return { ...state, userData: action.payload };
   }
 
-  return state
-
-}
+  return state;
+};
 
 const userPhoto = (state = photo, action) => {
-  
-}
-
+  switch (action.type) {
+    case USER_PHOTO:
+      return {...state, userPhoto: action.payload}
+      default:
+        return state;
+  }
+};
 
 /* reduder들 combine */
-const reducer = combineReducers({ 
-  postReducer, 
-  bundleReducer, 
+const reducer = combineReducers({
+  postReducer,
+  bundleReducer,
   memberReducer,
   userCardReducer,
   scrapReducer,
 });
 
-export default reducer
+export default reducer;
