0994f215d1d5dc9abaf98ce967df7c76fbafce43
bellasimi <WorkSY1111@gmail.com>
2022-03-06 20:24:40 +0900

    Chore: 카드 reducer 분리 취소

--- client/src/containers/DetailContainer.jsx
+++ client/src/containers/DetailContainer.jsx
@@ -6,17 +6,27 @@ import ApiService from '../ApiService'
 import {
   GET_DETAIL
    } from '../redux/modules/types'
-function DetailContainer({db}) {
+function DetailContainer() {
 
+  let data = useSelector( ({postReducer}) => { return postReducer });
+
+  const { cardId } = useParams();
+
+  const db = data.find(d => d.id === Number(cardId));
+  //console.log(db);
+
+/* 카드를 분리하면 cardId 달라질 때마다 card state가 변경되니까... 계속 초기화됨
   const dispatch = useDispatch();
 
   useEffect(()=>{
     dispatch({type: GET_DETAIL, payload: db })
-    //console.log(db)
+    console.log(db)
   },[])
 
+  const card = useSelector ( ({cardReducer}) => { return cardReducer })
+ */
 
-  return <Detail/>;
+  return <Detail card ={db}/>;
 }
 
 export default DetailContainer;
--- client/src/pages/DetailPage.jsx
+++ client/src/pages/DetailPage.jsx
@@ -1,21 +1,14 @@
 import DetailContainer from '../containers/DetailContainer';
 import SimilarContainer from '../containers/SimilarContainer';
 import { useParams } from 'react-router-dom';
-import { useSelector,useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import ApiService from '../ApiService'
 
 function DetailPage() {
 
-  let data = useSelector( ({postReducer}) => { return postReducer });
-
-  const { cardId } = useParams();
-
-  const db = data.find(d => d.id === Number(cardId));
-  //console.log(db);
-
   return (
     <>
-      <DetailContainer db = {db}/>
+      <DetailContainer />
       <SimilarContainer />
     </>
   );
